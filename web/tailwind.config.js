c9808a3e301cac064fde89aaa7ad721b879f1c0f
Amy Qin <amyqin501@gmail.com>
2023-05-29 12:37:00 -0400

    Week 2

---
+++ web/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'blueTheme': "url('../public/background-1.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
+
